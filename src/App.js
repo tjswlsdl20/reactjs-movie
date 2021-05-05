f9eaba7311a791ae95420ae4af3ccbe1eae8efd7
sjk <tjswlsdl20@gmail.com>
2021-05-05 16:53:07 +0900

    adding css

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import "./App.css";
 
 function App() {
-  let [star, starupdate] = useState(0);
+
   let [movie, movieUpdate] = useState([]);
   useEffect(() => {
     async function fetchData() {
       // You can await here
       const dada = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json');
 
-      movieUpdate(dada.data.data.movies);
+      movieUpdate([...dada.data.data.movies]);
 
       // ...
     }
     fetchData();
   }, []);
 
-
   return (
     <div>
-      <h1><span onClick={() => { starupdate(star + 1) }}>😁</span>{star}</h1>
-      {movie.map(info => (
-        <Movie key={info.id}
-          id={info.id}
-          year={info.year}
-          title={info.title}
-          summary={info.summary}
-          poster={info.medium_cover_image} />
-      ))}
-
-    </div>
+      <section class="container">
+        <div className="movies">
+          {movie.map(info => (
+            <Movie key={info.id}
+              id={info.id}
+              year={info.year}
+              title={info.title}
+              summary={info.summary}
+              poster={info.medium_cover_image}
+              genres={info.genres} />
+          ))}
+        </div>
+      </section>
+    </div >
   );
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-function Movie({ id, year, title, summary, poster }) {
-    return (<div class="movie">
-        <img src={poster} alt={title} title={title} />
-        <div class="movie__data">
-            <h3 class="movie__title">{title}</h3>
-            <h5 class="movie__year">{year}</h5>
-            <p class="movie__summary">{summary}</p>
-        </div>
-    </div>);
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
 
+function Movie({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
+            </div>
+        </div>
+    );
 }
 
 Movie.propTypes = {
@@ -17,7 +27,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
-export default Movie
+export default Movie;
